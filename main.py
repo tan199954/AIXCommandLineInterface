a7a833abaef8ff73b94af8f5ed980762162f4f84
unknown <nguy3nt4n99@gmail.com>
2023-01-15 22:19:02 +0700

    update main

--- main.py
+++ main.py
@@ -1,46 +1,47 @@
 from PySide6 import QtCore
 import sys
-import os
-currentPath = os.getcwd()
-detectProcessPath = os.path.join(currentPath,"DetectProcess") 
-trainProcessPath = os.path.join(currentPath,"TrainProcess") 
-sys.path.append(detectProcessPath)
-sys.path.append(trainProcessPath)
-
 import argparse
+import signal
+signal.signal(signal.SIGINT, signal.SIG_DFL)
 
+from InitProcess.Src.Api import InitProcess
+from InitProcess.Src.Core import TrainType
 from TrainProcess.Src.Api import TrainProcess
 from DetectProcess.Src.Api import DetectProcess
+
 
 def parse_opt():
     parser = argparse.ArgumentParser()
     subparsers = parser.add_subparsers(required=True)
 
+    initParser = subparsers.add_parser("init",help="initial initialize process")
+    initParser.add_argument("-t","--typeOfTrain",type=TrainType,help="'Box' or 'BBox' or 'Seg'")
+    initParser.add_argument("-i","--imagePath",type=str,help="full/path/to/images dataset directory")
+    initParser.add_argument("-l","--labelPath",type=str,help="full/path/to/labels dataset directory")
+    initParser.set_defaults(func=Init)
+
     trainParser = subparsers.add_parser("train",help="initial train process")
-    trainParser.add_argument("-tr","--trainPath",type=str,help="full/path/to/train dataset directory")
-    trainParser.add_argument("-val","--validPath",type=str,help="full/path/to/valid dataset directory")
-    trainParser.add_argument("-out","--outputPath",type=str,help="full/path/to/output directory")
     trainParser.set_defaults(func=Train)
 
     detectParser = subparsers.add_parser('detect', help='initial detect process')
     detectParser.add_argument("-s","--source",type=str,help="full/path/to/image or video or ipAdress/port")
-    detectParser.add_argument("-m","--modelPath",type=str,help="full/path/to/modelFile")
-    detectParser.add_argument("-t","--type",type=str,help="")
     detectParser.set_defaults(func=Detect)
 
     return parser
 
-def Train(trainPath,validPath, outputPath):
-     if trainPath is None or validPath is None or outputPath is None: 
-          raise argparse.ArgumentTypeError('usage: AIXCLI.exe train -h \nmissed some arguments ')
-     train = TrainProcess(trainPath,validPath,outputPath)
+def Train():
+     train = TrainProcess()
      train.execute()
-
-def Detect(source, modelPath,type):
-     if source is None or modelPath is None or type is None: 
+def Detect(source):
+     if source is None: 
           raise argparse.ArgumentTypeError('usage: AIXCLI.exe detect -h \nmissed some arguments')
-     detect = DetectProcess(source,modelPath,type)
-     detect.execute()
+     detect = DetectProcess(source)
+     detect.execute()      
+def Init(labelPath,imagePath,typeOfTrain):
+     if imagePath is None or labelPath is None or typeOfTrain is None:
+         raise argparse.ArgumentTypeError('usage: AIXCLI.exe init -h \nmissed some arguments')
+     init = InitProcess(imagePath,labelPath,typeOfTrain)
+     init.execute()
 
 def main(parser: argparse.ArgumentParser):
      args = parser.parse_args() 
