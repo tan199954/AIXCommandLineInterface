4395b1d2daf57b0b08cdfdfa3fca1a0ffa50145b
Tan chicken eat paddy <nguy3nt4n99@gmail.com>
2023-01-18 16:54:55 +0700

    main.py for python 3.6

--- main.py
+++ main.py
@@ -12,12 +12,12 @@ from DetectProcess.Src.Api import DetectProcess
 
 def parse_opt():
     parser = argparse.ArgumentParser()
-    subparsers = parser.add_subparsers(required=True)
+    subparsers = parser.add_subparsers()
 
     initParser = subparsers.add_parser("init",help="initial initialize process")
     initParser.add_argument("-t","--typeOfTrain",type=TrainType,help="'Box' or 'BBox' or 'Seg'")
-    initParser.add_argument("-i","--imagePath",type=str,help="full/path/to/images dataset directory")
-    initParser.add_argument("-l","--labelPath",type=str,help="full/path/to/labels dataset directory")
+    initParser.add_argument("-i","--imagePath",type=str,help="full/path/to/images directory")
+    initParser.add_argument("-l","--labelPath",type=str,help="full/path/to/labels directory")
     initParser.set_defaults(func=Init)
 
     trainParser = subparsers.add_parser("train",help="initial train process")
@@ -30,18 +30,27 @@ def parse_opt():
     return parser
 
 def Train():
+     sys.stdout.write("Waiting for the training...\n")
+     sys.stdout.flush()
      train = TrainProcess()
      train.execute()
 def Detect(source):
      if source is None: 
           raise argparse.ArgumentTypeError('usage: AIXCLI.exe detect -h \nmissed some arguments')
+     sys.stdout.write("Waiting for the detetion...\n")
+     sys.stdout.flush()
      detect = DetectProcess(source)
      detect.execute()      
 def Init(labelPath,imagePath,typeOfTrain):
      if imagePath is None or labelPath is None or typeOfTrain is None:
          raise argparse.ArgumentTypeError('usage: AIXCLI.exe init -h \nmissed some arguments')
+     sys.stdout.write("Waiting for the initialization...\n")
+     sys.stdout.flush()
      init = InitProcess(imagePath,labelPath,typeOfTrain)
      init.execute()
+     sys.stdout.write("Initialization is Finished\n")
+     sys.stdout.flush()
+     sys.exit()
 
 def main(parser: argparse.ArgumentParser):
      args = parser.parse_args() 
@@ -50,7 +59,6 @@ def main(parser: argparse.ArgumentParser):
      args_.pop('func', None)
      args.func(**args_)
 
-     
 if __name__ == "__main__":
      app = QtCore.QCoreApplication(sys.argv)
      main(parse_opt())
